0245e4f327539b48ea56dae41507ab33a14a779b
gvsumanth10 <93488982+gvsumanth10@users.noreply.github.com>
2022-12-27 15:15:37 +0530

    Add files via upload

---
+++ src/main/java/com/webapp/demo/TravelListApplication.java
@@ -0,0 +1,13 @@
+package com.webapp.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelListApplication.class, args);
+	}
+
+}
